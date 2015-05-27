8621eb2b937d4ad86ab7f89c71e6a40a0cc7e149
dmcbeing <dmcbeing@users.noreply.github.com>
2015-05-28 02:38:25 +0300

    Made some additions to elliminate the following security issues:
    Password was sent in plaintext over the internet, changed to use the hash instead.
    Protocol was prone to replay attacks, added a nonce to counter against it.

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	/* For license information please read the LICENSE file */
 
 	/* Copyright (c) 2015 Antonios A. Chariton <daknob@tolabaki.gr> */
+	/* Additional modifications by dmcbeing */
 
 	$PASSWORD = "No SHA-256 will match this phrase"; /* sha256($password); */
 	$WEBSITE  = "https://daknob.net/";	/* Website for redirects */
@@ -12,9 +13,13 @@
 		exit();
 	}
 
+	if(session_status()!=PHP_SESSION_ACTIVE)
+		session_start();												/* Start a session */
+
 	if(isset($_POST['action']) && $_POST['action'] == "action"){		/* Check if the form is submitted */
 		$saveTo = "../STORAGE/" . $_FILES['filename']['name'];			/* Generate the path the file's supposed to be saved in */
-		if(hash("sha256", $_POST['pass']) == $PASSWORD){				/* Check if the password is correct */
+		$phash = hash("sha256",$PASSWORD . strval(session_id()));		/* Calculate wanted hash(hash(pass)+session_id)*/
+		if($_POST['pass'] == $phash){									/* Check if the password is correct */
 			unset($_POST['pass']);										/* Attempt to remove password from memory */
 			if(is_dir("../STORAGE") === FALSE){							/* If it's the first time this runs, do some filesystem initialization */
 				mkdir("../STORAGE");
@@ -29,20 +34,41 @@
 				$saveTo = $saveTo . ".n";
 			}
 			if(move_uploaded_file($_FILES['filename']['tmp_name'], $saveTo)){	/* Try to 'upload' the file */
-				print("<h1 class='suc'>File upload completed.</h1>");	/* File uploaded successfully */ 
+				print("<h1 class='suc'>File upload completed.</h1>");	/* File uploaded successfully */
 			}else{
 				print("<h1>An error occured while uploading the file.</h1>");
 			}
 		}else{
-			print("<h1>An error occured while uploading file. Wrong password.</h1>");
+ 			print("<h1>An error occured while uploading file. Wrong password.</h1>");
 		}
 	}
+	session_regenerate_id();											/* Change the session_id */
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-		<title><?php print get_current_user(); ?>'s File Uploader</title>	
+		<script type="text/javascript" src="sha256.js"></script>
+		<script>
+			session="<?php print session_id(); ?>"
+
+			function genSHA256(input)
+			{
+				var hashObj = new jsSHA(input, "TEXT");
+				output=hashObj.getHash("SHA-256","HEX",1);
+				return output;
+			}
+
+			function hashPass()
+			{
+				var pass = document.getElementById("pass").value;
+				pass=genSHA256(pass);									/* Generate hash of password. */
+				pass=genSHA256(pass+session);							/* Concatenate hash(pass) and nonce*/
+				document.getElementById("pass").value = pass;			/* Send the nonced hash */
+				return true;
+			}
+		</script>
+		<title><?php print get_current_user(); ?>'s File Uploader</title>
 		<style>
 			.suc{
 				color:#282;
@@ -106,14 +132,15 @@
 		</style>
 	</head>
 	<body>
+
 		<div class="bodi">
 			<img src="logo.png" height="200px"/>
-			<form action="." method="post" enctype="multipart/form-data">
+			<form action="." method="post" enctype="multipart/form-data" onSubmit="return hashPass()">
 				<span class="filed">File: </span>
 				<input type="file" name="filename" id="filename"><br/>
 				<input type="hidden" name="action" value="action">
 				<input type="password" name="pass" id="pass">
-  		    	<input type="submit" value="Upload" name="submit" id="submit"> 
+  		    	<input type="submit" value="Upload" name="submit" id="submit">
 			</form>
 			<br>
 			<center><span style="text-align:center; margin-left:auto;margin-right:auto;font: 12px Helvetica; color:#666;">Copyright &copy; 2015 - Antonios A. Chariton &lt;daknob@tolabaki.gr&gt;</span></center>
